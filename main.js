9eac149664518184e3a2ebbe1eda2d553565071b
Saashvath <74235537+Saashvath@users.noreply.github.com>
2021-07-19 13:51:49 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,15 @@
+Webcam.set({
+    width:325,
+    height:325,
+    image_format:'png',
+    png_quality:100,
+    constraints:{facingMode:'Environment'}
+})
+
+
+
+
+Webcam.attach("#mobilediv")
+Webcam.attach("#mobilediv2")
+Webcam.attach("#microsoftdiv")
+Webcam.attach("#microsoftdiv2")
